3a1398d5a8ca0a4931857abc58f187ad999fad5a
Daniel Olsson <daniel@docode.se>
2025-12-31 11:41:30 +0100

    feat: update notification types

--- src/notification-types.ts
+++ src/notification-types.ts
@@ -1,3 +1,5 @@
+import { GoalType } from './goal-types';
+
 /**
  * Notification types for FCM push notifications
  */
@@ -7,9 +9,11 @@ export enum NotificationType {
   CHALLENGE_ACCEPTED = 'challenge_accepted',
   CHALLENGE_DECLINED = 'challenge_declined',
   CHALLENGE_ENDED = 'challenge_ended',
+  CHALLENGE_EARLY_START = 'challenge_early_start',
   NEW_LOG_ENTRY = 'new_log_entry',
   GOAL_ACHIEVED = 'goal_achieved',
   GOALS_SUBMITTED = 'goals_submitted',
+  CHEER = 'cheer',
 }
 
 /**
@@ -17,8 +21,29 @@ export enum NotificationType {
  */
 export interface NotificationPayload {
   type: NotificationType;
-  challengeId?: number;
-  userId?: number;
-  message?: string;
-  [key: string]: any;
+  timestamp: string;
+
+  // Challenge-related
+  challengeId?: string;
+
+  // User info (for invites, accepts, declines, goals submitted)
+  inviterId?: string;
+  inviterName?: string;
+  accepterId?: string;
+  accepterName?: string;
+  declinerId?: string;
+  declinerName?: string;
+  submitterId?: string;
+  submitterName?: string;
+
+  // Activity logging
+  loggerUserId?: string;
+  loggerName?: string;
+  activityType?: GoalType;
+  activityDescription?: string;
+
+  // Cheer notifications
+  logId?: string;
+  cheererId?: string;
+  cheererName?: string;
 }
